8bae04a9980a5aeaaa7feb19f5f492c64efa700d
anjukakoralage <anjukako@gmail.com>
2020-08-08 02:39:30 +0530

    Login Options Screen

---
+++ app/src/main/java/com/example/anjuka_koralage/downsouthtagsidriver/LoginOptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.anjuka_koralage.downsouthtagsidriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginOptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_option);
+    }
+}
